723422f23718f0456e52e4e2590fdf8cf47f6009
Godzilla810 <128792917+Godzilla810@users.noreply.github.com>
2023-11-24 00:54:20 +0800

    Add Player Interact to Counters
    #Bug to fix : Look rotation viewing vector is zero

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,15 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    public event EventHandler OnInteractAction;
+
     private PlayerInputControl playerInputControl;
     private void Awake()
     {
         playerInputControl = new PlayerInputControl();
         playerInputControl.Player.Enable();
+
+        playerInputControl.Player.Interact.performed += Interact_performed;
     }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
+    }
+
     public Vector2 GetMovementVectorNormalized()
     {
         Vector2 inputVector = playerInputControl.Player.Move.ReadValue<Vector2>();
