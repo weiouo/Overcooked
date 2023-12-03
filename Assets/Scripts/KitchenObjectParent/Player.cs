c5f2ec797f3db1e4e95ee875d62079dfe2140ee3
Tingouoww <150062918+Tingouoww@users.noreply.github.com>
2023-12-03 17:08:32 +0800

    選取變色

--- Assets/Scripts/KitchenObjectParent/Player.cs
+++ Assets/Scripts/KitchenObjectParent/Player.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
 
 public class Player : MonoBehaviour, IKitchenObjectParent
 {
+    public static Player Instance { get; private set; }
+
+    public event EventHandler<OnSelectedEventArgs> OnSelectedCounter;
+    public class OnSelectedEventArgs : EventArgs
+    {
+        public BaseCounter SelectedCounter;
+    }
+
     [SerializeField] private GameInput gameInput;
     [SerializeField] private LayerMask counterLayerMask;
     [SerializeField] private Transform holdPoint;
     [SerializeField] private float moveSpeed = 7.0f;
     [SerializeField] private float rotate_speed = 25.0f;
     [SerializeField] private float interactDistance = 1f;
+    public  Animator animator;
     private Vector3 interactDir;
     private bool isWalking;
     private KitchenObject KitchenObject;
+    private BaseCounter SelectedCounter;
+
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Debug.LogError("There is more than one Player");
+        }
+        Instance = this;
+    }
     private void Start()
     {
         gameInput.OnInteractAction += GameInput_OnInteractAction;
@@ -20,7 +42,9 @@ public class Player : MonoBehaviour, IKitchenObjectParent
     void Update()
     {
         PlayerMovement();
+        PlayerInteract();
     }
+
     //按e執行Interact
     private void GameInput_OnInteractAction(object sender, System.EventArgs e)
     {
@@ -28,16 +52,50 @@ public class Player : MonoBehaviour, IKitchenObjectParent
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         if (moveDir != Vector3.zero)
         {
-            interactDir = moveDir;
+             interactDir = moveDir;
         }
-        //Debug.DrawRay(transform.position, interactDir * interactDistance, Color.red);
+        Debug.DrawRay(transform.position, interactDir * interactDistance, Color.red);
         if (Physics.Raycast(transform.position, interactDir, out RaycastHit raycastHit, interactDistance, counterLayerMask))
         {
             if (raycastHit.transform.TryGetComponent(out BaseCounter basecounter))
             {
                 basecounter.Interact(this);
+           }
+        }
+    }
+    private void PlayerInteract()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        if (moveDir != Vector3.zero)
+        {
+            interactDir = moveDir;
+        }
+        Debug.DrawRay(transform.position, interactDir * interactDistance, Color.red);
+        if (Physics.Raycast(transform.position, interactDir, out RaycastHit raycastHit, interactDistance, counterLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out BaseCounter basecounter))
+            {
+                if (basecounter != SelectedCounter)
+                {
+                    SetSelectedCounter(basecounter);
+                }
+            }
+            else
+            {
+                SetSelectedCounter(null);
             }
         }
+        else
+        {
+            SetSelectedCounter(null);
+        }
+    }
+    private void SetSelectedCounter(BaseCounter selectedCounter)
+    {
+        this.SelectedCounter = selectedCounter;
+
+        OnSelectedCounter?.Invoke(this, new OnSelectedEventArgs { SelectedCounter = selectedCounter });
     }
     private void PlayerMovement()
     {
@@ -51,6 +109,15 @@ public class Player : MonoBehaviour, IKitchenObjectParent
         transform.position += moveDir * moveSpeed * Time.deltaTime;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotate_speed);
         isWalking = moveDir != Vector3.zero;
+
+        if (isWalking)
+        {
+            animator.SetBool("Walk", true);
+        }
+        else
+        {
+            animator.SetBool("Walk", false);
+        }
     }
 
     //定義KitchenObjectParent介面
