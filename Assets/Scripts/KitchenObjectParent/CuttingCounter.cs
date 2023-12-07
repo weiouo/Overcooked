24575fb7698c3929b00848b2e1dc8ef8ca25a91d
Godzilla810 <128792917+Godzilla810@users.noreply.github.com>
2023-12-07 22:40:39 +0800

    Improve CuttingCounter script

--- Assets/Scripts/KitchenObjectParent/CuttingCounter.cs
+++ Assets/Scripts/KitchenObjectParent/CuttingCounter.cs
@@ -5,39 +5,16 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using System;
 
-public class CuttingCounter : BaseCounter
+public class CuttingCounter : ClearCounter
 {
     private Ingredient ingredient;
     public delegate void CutEventHandler(bool isCutting);
     public static event CutEventHandler CutBool;
 
-    public override void Interact(Player player)
-    {
-        //桌上沒東西
-        if (!HasKitchenObject())
-        {
-            //玩家有東西 & 該東西是食材
-            if (player.HasKitchenObject() & player.GetKitchenObject() is Ingredient)
-            {
-                //放食材
-                ingredient = player.GetKitchenObject() as Ingredient;
-                player.GetKitchenObject().SetKitchenObjectParent(this);
-            }
-        }
-        //桌上有東西
-        else
-        {
-            //玩家沒東西
-            if (!player.HasKitchenObject())
-            {
-                //拿食材
-                ingredient = null;
-                this.GetKitchenObject().SetKitchenObjectParent(player);
-            }
-        }
-    }
     public override void Cut()
     {
+        //試圖獲取該食材
+        ingredient = this.GetKitchenObject() is Ingredient ? this.GetKitchenObject() as Ingredient : null;
         //有食材才能切
         if (ingredient != null && ingredient.CanCut())
         {
--- Assets/Scripts/KitchenObjectParent/Dishrack.cs
+++ Assets/Scripts/KitchenObjectParent/Dishrack.cs
@@ -4,5 +4,45 @@ using UnityEngine;
 
 public class Dishrack : BaseCounter
 {
-    
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private int plateCount;
+    [SerializeField] private float generateTime;
+    private GameObject plate;
+    private int count;
+    private float time;
+    public void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= generateTime)
+        {
+            time = 0;
+            if (count < plateCount)
+            {
+                GeneratePlateVisual(count);
+                count++;
+            }
+            else
+            {
+                Debug.Log("full");
+            }
+        }
+    }
+    private void GeneratePlateVisual(int plateIndex)
+    {
+        Transform startPoint = GetPoint();
+        Vector3 offset = new Vector3(-0.25f * plateIndex, 0, 0);
+        plate = Instantiate(prefab, startPoint.position + offset, Quaternion.Euler(0, 0, 90));
+    }
+    public override void Interact(Player player)
+    {
+        //玩家沒東西
+        if (!player.HasKitchenObject() && count != 0)
+        {
+            count--;
+            Destroy(plate);
+            //給玩家東西
+            KitchenObject kitchenObject = Instantiate(prefab).GetComponent<KitchenObject>();
+            kitchenObject.SetKitchenObjectParent(player);
+        }
+    }
 }
