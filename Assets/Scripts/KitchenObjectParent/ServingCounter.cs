072528bdbf6c0d3defb6eea6676299c966a7b9d2
Tingouoww <150062918+Tingouoww@users.noreply.github.com>
2023-12-12 18:06:51 +0800

    servingCounter

--- Assets/Scripts/KitchenObjectParent/ServingCounter.cs
+++ Assets/Scripts/KitchenObjectParent/ServingCounter.cs
@@ -7,8 +7,9 @@ public class ServingCounter : BaseCounter
     //¥u¥á¡AµL§PÂ_
     public override void Interact(Player player)
     {
-        if (player.GetKitchenObject() is Plate)
+        if (player.GetKitchenObject() is Plate plate)
         {
+            ServingManager.Instance.ServingRecipeCorrect(plate);
             player.GetKitchenObject().DestroySelf();
         }
     }
---
+++ Assets/Scripts/ScriptableObject/RecipeSOList.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+//所有菜單形成的列表
+public class RecipeSOList : ScriptableObject
+{
+    public List<RecipeSO> recipeSOList;
+}
